151dbed7e66c0a1ca2c71f487e1f64af2e186112
Fernando Reyes <luisfernandoreyes45@outlook.com>
2023-05-24 11:53:53 -0600

    Se agrega el proyecto

---
+++ 026_SistemaCarritoCompras-main/ProyectoTest/Controllers/HomeController.cs
@@ -0,0 +1,311 @@
+﻿using ProyectoTest.Models;
+using ProyectoTest.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ProyectoTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(bool? Administrador)
+        {
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true )
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        private bool ObtenerAdministrador()
+        {
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null)
+            {
+                if (oUsuario.EsAdministrador == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public ActionResult About()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Categoria()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Marca()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Producto()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Preguntasfrecuentes()
+        {
+            bool Administrador = ObtenerAdministrador();
+            Administrador = TempData["Administrador"] as bool? ?? Administrador;
+            Usuario oUsuario = new Usuario();
+            if (Session["Usuario"] != null && Administrador == true)
+            {
+                ViewBag.MostrarPestañas = true;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+            else
+            {
+                ViewBag.MostrarPestañas = false;
+                TempData["Administrador"] = Administrador;
+                return View();
+            }
+        }
+
+        public ActionResult Tienda()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public JsonResult ListarCategoria() {
+            List<Categoria> oLista = new List<Categoria>();
+            oLista = CategoriaLogica.Instancia.Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarCategoria(Categoria objeto)
+        {
+            bool respuesta = false;
+            respuesta = (objeto.IdCategoria == 0) ? CategoriaLogica.Instancia.Registrar(objeto) : CategoriaLogica.Instancia.Modificar(objeto);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarCategoria(int id)
+        {
+            bool respuesta = false;
+            respuesta = CategoriaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [HttpGet]
+        public JsonResult ListarMarca()
+        {
+            List<Marca> oLista = new List<Marca>();
+            oLista = MarcaLogica.Instancia.Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarMarca(Marca objeto)
+        {
+            bool respuesta = false;
+            respuesta = (objeto.IdMarca == 0) ? MarcaLogica.Instancia.Registrar(objeto) : MarcaLogica.Instancia.Modificar(objeto);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarMarca(int id)
+        {
+            bool respuesta = false;
+            respuesta = MarcaLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [HttpGet]
+        public JsonResult ListarProducto()
+        {
+            List<Producto> oLista = new List<Producto>();
+
+            oLista = ProductoLogica.Instancia.Listar();
+            oLista = (from o in oLista
+                      select new Producto()
+                      {
+                          IdProducto = o.IdProducto,
+                          Nombre = o.Nombre,
+                          Descripcion = o.Descripcion,
+                          oMarca = o.oMarca,
+                          oCategoria = o.oCategoria,
+                          Precio = o.Precio,
+                          Stock = o.Stock,
+                          RutaImagen = o.RutaImagen,
+                          base64 = utilidades.convertirBase64(Server.MapPath(o.RutaImagen)),
+                          extension = Path.GetExtension(o.RutaImagen).Replace(".", ""),
+                          Activo = o.Activo
+                      }).ToList();
+
+            JsonResult jsonResult = Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+
+        [HttpPost]
+        public JsonResult GuardarProducto(string objeto, HttpPostedFileBase imagenArchivo)
+        {
+
+            Response oresponse = new Response() { resultado = true, mensaje = "" };
+
+            try
+            {
+                Producto oProducto = new Producto();
+                oProducto = JsonConvert.DeserializeObject<Producto>(objeto);
+
+                string GuardarEnRuta = "~/Imagenes/Productos/";
+                string physicalPath = Server.MapPath("~/Imagenes/Productos");
+
+                if (!Directory.Exists(physicalPath))
+                    Directory.CreateDirectory(physicalPath);
+
+                if (oProducto.IdProducto == 0)
+                {
+                    int id = ProductoLogica.Instancia.Registrar(oProducto);
+                    oProducto.IdProducto = id;
+                    oresponse.resultado = oProducto.IdProducto == 0 ? false : true;
+
+                }
+                else
+                {
+                    oresponse.resultado = ProductoLogica.Instancia.Modificar(oProducto);
+                }
+
+                
+                if (imagenArchivo != null && oProducto.IdProducto != 0)
+                {
+                    string extension = Path.GetExtension(imagenArchivo.FileName);
+                    GuardarEnRuta = GuardarEnRuta + oProducto.IdProducto.ToString() + extension;
+                    oProducto.RutaImagen = GuardarEnRuta;
+
+                    imagenArchivo.SaveAs(physicalPath + "/" + oProducto.IdProducto.ToString() + extension );
+
+                    oresponse.resultado = ProductoLogica.Instancia.ActualizarRutaImagen(oProducto);
+                }
+
+            }
+            catch (Exception e)
+            {
+                oresponse.resultado = false;
+                oresponse.mensaje = e.Message;
+            }
+            
+            JsonResult jsonResult = Json(oresponse, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+        [HttpPost]
+        public JsonResult EliminarProducto(int id)
+        {
+            bool respuesta = false;
+            respuesta = ProductoLogica.Instancia.Eliminar(id);
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Response {
+
+        public bool resultado { get; set; }
+        public string mensaje { get; set; }
+    }
+}
